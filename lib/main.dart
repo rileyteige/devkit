b275575325fe920b696c4dfac4f32d145a35f200
Riley Teige <riley@teige.io>
2023-08-30 17:07:32 -0600

    Remove route animation

--- lib/main.dart
+++ lib/main.dart
@@ -18,23 +18,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Riley's Dev Kit",
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.blue,
           brightness: Brightness.dark,
@@ -43,7 +28,7 @@ class MyApp extends StatelessWidget {
       ),
       navigatorKey: navigatorKey,
       initialRoute: Routes.initial,
-      routes: Routes.all,
+      onGenerateRoute: Routes.onGenerateRoute,
       builder: (context, child) {
         return Builder(
           builder: (context) {
@@ -69,6 +54,10 @@ class MyApp extends StatelessWidget {
                         NavigationListTile(
                           route: Routes.hashes,
                           title: Text('Hashes'),
+                        ),
+                        NavigationListTile(
+                          route: Routes.regexTester,
+                          title: Text('Regex'),
                         ),
                       ],
                     );
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,5 @@
 import 'package:devkit/image_resizer.dart';
+import 'package:devkit/regex_tester.dart';
 import 'package:flutter/material.dart';
 
 import 'hashes.dart';
@@ -11,11 +12,27 @@ class Routes {
   static const String imageResizer = '/ImageResizer';
   static const String stringTransformations = '/StringTransformations';
   static const String hashes = '/Hashes';
+  static const String regexTester = '/RegexTester';
 
   static final Map<String, WidgetBuilder> all = {
-    home: (context) => const Center(child: Text('Choose a tool')),
-    imageResizer: (context) => const ImageResizer(),
-    stringTransformations: (context) => const StringTransformations(),
-    hashes: (context) => const Hashes(),
+    home: (_) => const Center(child: Text('Choose a tool')),
+    imageResizer: (_) => const ImageResizer(),
+    stringTransformations: (_) => const StringTransformations(),
+    hashes: (_) => const Hashes(),
+    regexTester: (_) => const RegexTester(),
   };
+
+  static Route onGenerateRoute(RouteSettings settings) {
+    final name = settings.name;
+
+    return PageRouteBuilder(
+      transitionDuration: Duration.zero,
+      reverseTransitionDuration: Duration.zero,
+      pageBuilder: (_, __, ___) => switch (name) {
+        home => const Center(child: Text('Choose a tool')),
+        imageResizer => const ImageResizer(),
+        _ => Center(child: Text('Route not found: $name')),
+      },
+    );
+  }
 }
