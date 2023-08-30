9e96a14ee89aa3a050fddea0fa53ef965bbc779f
Riley Teige <riley@teige.io>
2023-08-30 17:07:18 -0600

    Add RegexTester view

---
+++ lib/regex_tester.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegexTester extends StatefulWidget {
+  const RegexTester({super.key});
+
+  @override
+  State<RegexTester> createState() => _RegexTesterState();
+}
+
+class _RegexTesterState extends State<RegexTester> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Coming Soon'));
+  }
+}
