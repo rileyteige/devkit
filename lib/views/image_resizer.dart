ca6ac0ae8f7266c4c7ae417fb631b004e1078465
Riley Teige <riley@teige.io>
2024-06-13 10:11:44 -0500

    accommodate average vs cubic image interpolation

--- lib/views/image_resizer.dart
+++ lib/views/image_resizer.dart
@@ -66,19 +66,25 @@ Future<Uint8List?> _resizeImage(Map<String, dynamic> argMap) async {
     return null;
   }
 
-  if (decoded.format == image.Format.uint16) {
-    final uint8Bytes = image.encodeJpg(decoded);
-    decoded = image.decodeJpg(uint8Bytes);
-    if (decoded == null) {
-      debugPrint('_resizeImage - Failed to re-decode JPG image');
-      return null;
-    }
-  }
+  // if (decoded.format == image.Format.uint16) {
+  //   final uint8Bytes = image.encodeJpg(decoded);
+  //   decoded = image.decodeJpg(uint8Bytes);
+  //   if (decoded == null) {
+  //     debugPrint('_resizeImage - Failed to re-decode JPG image');
+  //     return null;
+  //   }
+  // }
 
-  final scaled = image.copyResize(decoded,
-      width: args.targetWidth,
-      height: args.targetHeight,
-      interpolation: image.Interpolation.cubic);
+  final interpolation = decoded.format == image.Format.uint16
+      ? image.Interpolation.average
+      : image.Interpolation.cubic;
+
+  final scaled = image.copyResize(
+    decoded,
+    width: args.targetWidth,
+    height: args.targetHeight,
+    interpolation: interpolation,
+  );
 
   return image.encodeJpg(scaled);
 }
